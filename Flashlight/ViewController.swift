2ac36bedd53c07c5553bae90fb74b4e1f9da0602
Chelsea Troy <chelsea.dommert@gmail.com>
2019-04-03 20:08:41 -0500

    Add target to outlet to create flashlight

--- Flashlight/ViewController.swift
+++ Flashlight/ViewController.swift
@@ -10,9 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lightSwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.black
+        
+        self.lightSwitch.addTarget(self, action: #selector(switchLight), for: .touchUpInside)
+    }
+    
+    @objc func switchLight() {
+        if self.view.backgroundColor == UIColor.black {
+            self.view.backgroundColor = UIColor.white
+        } else {
+            self.view.backgroundColor = UIColor.black
+        }
     }
 
 
